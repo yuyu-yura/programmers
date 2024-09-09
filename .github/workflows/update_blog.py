22419d390bee95b7c3d620a120f9019382ed8e91
yuyu__yura <106868246+yuyu-yura@users.noreply.github.com>
2024-09-09 09:09:01 +0900

    Update update_blog.py

--- .github/workflows/update_blog.py
+++ .github/workflows/update_blog.py
@@ -7,7 +7,7 @@ import os
 rss_url = 'https://api.velog.io/rss/@yuyu__yura'
 
 # 깃허브 레포지토리 경로
-repo_path = '.'
+repo_path = 'https://github.com/yuyu-yura/programmers'
 
 # 'velog-posts' 폴더 경로
 posts_dir = os.path.join(repo_path, 'velog-posts')
